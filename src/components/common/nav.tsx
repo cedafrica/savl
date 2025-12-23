245387788734dd88744c82e8865293091e3c19ed
cedafrica <marketing@ced.africa>
2025-12-23 06:33:33 +0100

    dsds

--- src/components/common/nav.tsx
+++ src/components/common/nav.tsx
@@ -75,13 +75,20 @@ const Nav: React.FC = () => {
 
           {/* LOGO */}
           <Link to="/" className="flex items-center">
-            <motion.img
-              src="/spectra-logo.svg"
-              alt="Spectra AVL"
-              className="w-70 sm:w-86"
-              animate={{ scale: scrolled ? 0.9 : 1 }}
-              transition={{ duration: 0.4 }}
-            />
+           <motion.img
+  src="/spectra-logo.svg"
+  alt="Spectra AVL"
+  className={`
+    w-[140px] sm:w-[200px]
+    transition-[width] duration-300
+    ${scrolled ? "sm:w-[180px]" : ""}
+  `}
+  style={{
+    imageRendering: "auto",
+    shapeRendering: "geometricPrecision",
+  }}
+/>
+
           </Link>
 
           {/* DESKTOP NAV */}
